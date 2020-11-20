18b1a43cfa67fc9bbdf7e5ce0a5ed6377a3324d6
Nathan Gawith <NathanGawith@gmail.com>
2020-11-20 17:08:28 -0600

    Added automation.js

--- web-store/b1topleftnew.pl
+++ web-store/b1topleftnew.pl
@@ -179,6 +179,29 @@ if($input{'user'} eq "staff") # Staff maintaining.
 </style>
 </head>
 <body $_[2]>
+<script>
+window.automatedInsertItem = ((category, title, sku, brand, price) => {
+    const myEles = document.querySelector(`[value="${category}"]`);
+    (myEles ? myEles.click() : undefined);
+    const setValue = (name, value) => {
+        const input = document.querySelector(`[name="${name}"]`);
+        (input ? input.value = value : undefined);
+    }
+    setValue('title', title);
+    setValue('stocknum', 1);
+    setValue('stockquantity', 1);
+    setValue('upc', sku);
+    setValue('_sku', sku);
+    setValue('content', `${title}\n${brand}`);
+    for (let i = 1; i <= 5; i++) {
+        setValue(`price${i}title`, `Price of ${i} ${title}`);
+        setValue(`price${i}`, (price * i).toFixed(2));
+        setValue(`quantity${i}`, i);
+    }
+    const btnSubmit = document.querySelector('[value="Submit"]');
+    btnSubmit.click();
+});
+</script>
 <script language="Javascript" src="$opimageprefix/allcity/allshop/common/a1utility.js"></script>
 <script language=javascript>
 ENDOFTEXT
